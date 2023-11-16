d0d3c63290e1c1c131c4219297f596821c452935
moisesrobles-04 <moises.robles@upr.edu>
2023-11-16 12:05:56 -0400

    Testing views

--- views.py
+++ views.py
@@ -1,2 +1,12 @@
 import sqlite3 as sql3
 
+
+def test():
+    a = sql3.connect("./data/grants.db")
+    b = a.cursor().execute("SELECT * FROM categories")
+
+    print(b)
+    a.close()
+
+if __name__ == "__main__":
+    test()
